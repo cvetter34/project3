be2aa6f28e150edcff11f32b6d9d3250ce3a2f73
JulesWiz <ngjulie0203@gmail.com>
2014-04-08 09:49:17 +0800

    Added tool model and execute the migration.

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -8,4 +8,18 @@ class ToolsController < ApplicationController
   def create
     render text: "Tools CREATEZ!!!"
   end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+
 end
